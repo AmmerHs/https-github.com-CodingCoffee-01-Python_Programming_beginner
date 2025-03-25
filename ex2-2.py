92734adab4535e610d31379a24b50cc4c201ec7d
AmmerHs <ammer050466@icloud.com>
2025-03-25 21:03:44 +0800

    Create ex2-2.py

---
+++ ex2-2.py
@@ -0,0 +1,16 @@
+'''
+# This is my first program
+
+'''
+-------------------------------------
+this is hell0 python program
+-------------------------------------
+'''
+
+print("hello") # This is my first program
+
+# This is my first program
+
+print(2+3)
+
+print(9*9)
