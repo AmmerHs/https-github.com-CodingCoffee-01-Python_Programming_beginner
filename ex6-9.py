8936122fcb7e22f75065db8ce947381f168f360d
AmmerHs <ammer050466@icloud.com>
2025-04-22 21:36:38 +0800

    Update ex6-9.py

--- ex6-9.py
+++ ex6-9.py
@@ -1,9 +1,9 @@
 '''
 1.輸入2個整數,求兩數的和共並輸出。
-1.輸入2個整數,求兩數的平方和共並輸出。x*x y+y
+1.輸入2個整數,求兩數的平方和共並輸出。x*x + y*y
+'''
 x = int(input("please input a num :"))
 y = int(input("please input a num :"))
-print("x=",x)
-print("y=",y)
-print("x+y=",x+y)
-'''
+print("x= ",x)
+print("y= ",y)
+print("x + y= ",x+y)
