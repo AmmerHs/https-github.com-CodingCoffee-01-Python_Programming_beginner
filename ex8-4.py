415dfff3fc5b23bb60361a9ac67d85d8ce9d5c86
AmmerHs <ammer050466@icloud.com>
2025-05-06 20:17:17 +0800

    Create ex8-4.py

---
+++ ex8-4.py
@@ -0,0 +1,15 @@
+'''
+CARS = ["FORD" , "VOLVO" ,"BMW"]
+
+#print (CARS[0])
+#print (CARS[1])
+#print (CARS[2])
+print(CARS)
+
+'''
+
+for i in range(0,3):
+    print(CARS[i])
+'''  
+for i in CARS:
+    print(i)
