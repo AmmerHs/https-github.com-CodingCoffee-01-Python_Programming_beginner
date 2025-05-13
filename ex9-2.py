04c9a0b95c1a29787bde99c2403d8b7a3c3ed6ac
AmmerHs <ammer050466@icloud.com>
2025-05-13 20:19:08 +0800

    Update ex9-2.py

--- ex9-2.py
+++ ex9-2.py
@@ -1,12 +1,12 @@
-class person:
-  def _init_(self, name, age):
+class Person:
+  def __init__(self, name, age):
     self.name = name
     self.age = age
-    
-  def myfunc(self):
-      print ("Hello my name is"+ self.name" my age is" +str (self.age))
-      #print("Hello my name is"+ self.name" my age is" , (self.age)
 
-p1 = person("John",36)
-print (p1)    
+  def myfunc(self):
+    print("Hello my name is " + self.name+ "my age is "+ str(self.age))
+    #print("Hello my name is " + self.name+ "my age is ", self.age)
+
+p1 = Person("John", 36)
+print (p1)
 p1.myfunc()
